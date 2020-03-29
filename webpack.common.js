369e9179430993fc8a177f78b91d75910b1fd204
Blaž Mandl <blaz.mandl@gmail.com>
2020-03-29 15:45:17 +0200

    Changed webpack configuration for production and development

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        contentBase: './dist'
-    },
+    entry: './src/index.js',    
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
